e5f2015e3960a59ce98d3b0521c14bbc206ed6b2
Jonathan Diego Rodríguez Rodríguez <jon_eldiablo@hotmail.com>
2018-07-02 17:02:37 -0500

    menu lateral & search

---
+++ template/src/_modules/search-general/search-general.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export default class SearchGeneral {
+  constructor() {
+    this.name = 'search-general';
+    console.log('%s module', this.name.toLowerCase());
+  }
+}
---
+++ template/src/_modules/search-general/tests/search-general.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import SearchGeneral from '../search-general';
+
+describe('SearchGeneral View', function() {
+
+  beforeEach(() => {
+    this.searchGeneral = new SearchGeneral();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.searchGeneral);
+  });
+
+});
