9ed83afcc1626b78b2af72e4050d9edafb0106d4
Jonathan Diego Rodríguez Rodríguez <jon_eldiablo@hotmail.com>
2018-07-05 03:05:48 -0500

    avance login

--- template/src/_scripts/main.js
+++ template/src/_scripts/main.js
@@ -2,16 +2,5 @@
 // Should handle bootstrapping/starting application
 
 (($, c8s) => {
-  components = {
-    panel: new c8s.Panel($, '.panel', {
-      collapsable: false,
-      slidereveal: {
-        autoEscape: false,
-        top: 0,
-        width: 162,
-        push: true,
-        //trigger
-      }
-    })
-  };
-})(jQuery, components);
+  let components = {};
+})(jQuery, modules);
